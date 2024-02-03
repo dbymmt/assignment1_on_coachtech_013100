d52fcb546b9b514054ff4a5f73132b3c00b888a9
dbymmt <dbymmt@dbtstdomoutno01.com>
2024-02-03 14:24:24 +0900

    in_progress_front_2402031424

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -11,54 +11,55 @@
 
     <form action="/create" method="POST">
     @csrf
-    <dl class="confirm-body-form">
-        <dt class="confirm-body__name-label" >お名前</dt>
-        <dd class="confirm-body__name">{{ $data['first_name']." ".$data['last_name'] }}</dd>
-        <input type="hidden" name="first_name" value="{{ $data['first_name'] }}">
-        <input type="hidden" name="last_name" value="{{ $data['last_name'] }}">
+        <dl class="confirm-body-form">
+            <dt class="confirm-body__name-label" >お名前</dt>
+            <dd class="confirm-body__name">{{ $data['first_name']." ".$data['last_name'] }}</dd>
+            <input type="hidden" name="first_name" value="{{ $data['first_name'] }}">
+            <input type="hidden" name="last_name" value="{{ $data['last_name'] }}">
 
-        <dt class="confirm-body__gender-label" >性別</dt>
-        <dd class="confirm-body__gender-men" >
-            {{
-                $data['gender'] === 1 ? '男性' :
-                ($data['gender'] === 2 ? '女性' : 'その他')}}
-        </dd>
-        <input type="hidden" name="gender" value="{{ $data['gender'] }}">
+            <dt class="confirm-body__gender-label" >性別</dt>
+            <dd class="confirm-body__gender-men" >
+                {{
+                    $data['gender'] === 1 ? '男性' :
+                    ($data['gender'] === 2 ? '女性' : 'その他')}}
+            </dd>
+            <input type="hidden" name="gender" value="{{ $data['gender'] }}">
 
-        <dt class="confirm-body__email-label" >メールアドレス</dt>
-        <dd class="confirm-body__email" >{{$data['email']}}</dd>
-        <input type="hidden" name="email" value="{{ $data['email'] }}">
+            <dt class="confirm-body__email-label" >メールアドレス</dt>
+            <dd class="confirm-body__email" >{{$data['email']}}</dd>
+            <input type="hidden" name="email" value="{{ $data['email'] }}">
 
-        <dt class="confirm-body__tel-label" >電話番号</dt>
-        <dd class="confirm-body__tel">{{$data['tel1'].$data['tel2'].$data['tel3']}}</dd>
-        <input type="hidden"  name="tel1" value="{{ $data['tel1'] }}">
-        <input type="hidden"  name="tel2" value="{{ $data['tel2'] }}">
-        <input type="hidden"  name="tel3" value="{{ $data['tel3'] }}">
+            <dt class="confirm-body__tel-label" >電話番号</dt>
+            <dd class="confirm-body__tel">{{$data['tel1'].$data['tel2'].$data['tel3']}}</dd>
+            <input type="hidden"  name="tel1" value="{{ $data['tel1'] }}">
+            <input type="hidden"  name="tel2" value="{{ $data['tel2'] }}">
+            <input type="hidden"  name="tel3" value="{{ $data['tel3'] }}">
 
-        <dt class="confirm-body__address-label">住所</dt>
-        <dd class="confirm-body__address">{{$data['address']}}</dd>
-        <input type="hidden" name="address" value="{{ $data['address'] }}">
+            <dt class="confirm-body__address-label">住所</dt>
+            <dd class="confirm-body__address">{{$data['address']}}</dd>
+            <input type="hidden" name="address" value="{{ $data['address'] }}">
 
-        <dt class="confirm-body__building-label">建物名</dt>
-        <dd class="confirm-body__building">{{$data['building']}}</dd>
-        <input type="hidden" name="building" value="{{ $data['building'] }}">
+            <dt class="confirm-body__building-label">建物名</dt>
+            <dd class="confirm-body__building">{{$data['building']}}</dd>
+            <input type="hidden" name="building" value="{{ $data['building'] }}">
 
-        <dt class="confirm-body__category-label" >お問い合わせの種類</dt>
-        <dd class="confirm-body__category">
-            {{$data['category_id'] === 1 ? '商品のお届けについて' :
-            ($data['category_id'] === 2 ? '商品の交換について' :
-            ($data['category_id'] === 3 ? '商品トラブル':
-            ($data['category_id'] === 4 ? 'ショップへのお問い合わせ': 'その他')))}}
-        </dd>
-        <input type="hidden" name="category_id" value="{{ $data['category_id'] }}">
+            <dt class="confirm-body__category-label" >お問い合わせの種類</dt>
+            <dd class="confirm-body__category">
+                {{$data['category_id'] === 1 ? '商品のお届けについて' :
+                ($data['category_id'] === 2 ? '商品の交換について' :
+                ($data['category_id'] === 3 ? '商品トラブル':
+                ($data['category_id'] === 4 ? 'ショップへのお問い合わせ': 'その他')))}}
+            </dd>
+            <input type="hidden" name="category_id" value="{{ $data['category_id'] }}">
 
-        <dt class="confirm-body__detail-label">お問い合わせの内容</dt>
-        <dd class="confirm-body__detail">{{$data['detail']}}</dd>
-        <input type="hidden" name="detail" value="{{ $data['detail'] }}">
-
-        <input type="submit" value="送信">
-        <input type="submit" name="back" value="修正">
+            <dt class="confirm-body__detail-label">お問い合わせの内容</dt>
+            <dd class="confirm-body__detail">{{$data['detail']}}</dd>
+            <input type="hidden" name="detail" value="{{ $data['detail'] }}">
+        </dl>
+        <div class="confirm-body__buttons">
+            <input type="submit" value="送信">
+            <input type="submit" name="back" value="修正">
+        </div>
     </form>
-    </dl>
 </article>
 @endsection
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -19,7 +19,7 @@
     <form action="/" method="POST">
     @csrf
         <div class="index-body-name">
-            <label for="index-body__first-name" class="index-body__first-name-label" >お名前</label>
+            <label for="index-body__first-name" class="index-body__first-name-label" >お名前<span class="index-body__asterisk">※</span></label>
             <div class="index-body__input-name">
                 <input type="text" id="index-body__first-name" name="first_name" value="{{old('first_name')}}">
                 <input type="text" id="index-body__last-name" name="last_name" value="{{old('last_name')}}">
@@ -27,7 +27,7 @@
         </div>
         
         <div class="index-body-gender">
-            <label for="index-body__gender" class="index-body__gender-label" >性別</label>
+            <label for="index-body__gender" class="index-body__gender-label" >性別<span class="index-body__asterisk">※</span></label>
             <div class="index-body__input-gender">
                 <input type="radio" id="index-body__gender-men" name="gender" value="1" {{ old('gender') === '1' ? 'checked' : '' }}>
                 <label for="index-body__gender-man" class="index-body__gender-label" >男性</label>
@@ -39,28 +39,25 @@
         </div>
 
         <div class="index-body-email">
-            <label for="index-body__first-email" class="index-body__email-label" >メールアドレス</label>
+            <label for="index-body__first-email" class="index-body__email-label" >メールアドレス<span class="index-body__asterisk">※</span></label>
             <div class="index-body__input-email">
                 <input type="text" id="index-body__email" name="email" value="{{old('email')}}">
             </div>
         </div>
 
         <div class="index-body-tel">
-            <label for="index-body__tel" class="index-body__tel-label" >電話番号</label>
+            <label for="index-body__tel" class="index-body__tel-label" >電話番号<span class="index-body__asterisk">※</span></label>
             <div class="index-body__input-tel">
-                {{-- <label for="index-body__tel-first" class="index-body__tel-label" ></label> --}}
                 <input type="text" id="index-body__tel-first" name="tel1" value="{{old('tel1')}}">
                 <span>-</span>
-                {{-- <label for="index-body__tel-second" class="index-body__tel-label" ></label> --}}
                 <input type="text" id="index-body__tel-second" name="tel2" value="{{old('tel2')}}">
                 <span>-</span>
-                {{-- <label for="index-body__tel-third" class="index-body__tel-label" ></label> --}}
                 <input type="text" id="index-body__tel-third" name="tel3" value="{{old('tel3')}}">
             </div>
         </div>
 
         <div class="index-body-addoress">
-            <label for="index-body__address" class="index-body__address-label" >住所</label>
+            <label for="index-body__address" class="index-body__address-label" >住所<span class="index-body__asterisk">※</span></label>
             <div class="index-body__input-address">
                 <input type="text" id="index-body__address" name="address" value="{{old('address')}}">
             </div>
@@ -74,9 +71,9 @@
         </div>
 
         <div class="index-body-category">
-            <label for="index-body__category" class="index-body__category-label" >お問い合わせの種類</label>
+            <label for="index-body__category" class="index-body__category-label" >お問い合わせの種類<span class="index-body__asterisk">※</span></label>
             <div class="index-body__input_category">
-                <select name="category_id">
+                <select id="index-body__category" name="category_id">
                     <option value="">選択してください</option>
                     <option value="1" @if((int)old('category_id') === 1) selected @endif>商品のお届けについて</option>
                     <option value="2" @if((int)old('category_id') === 2) selected @endif>商品の交換について</option>
@@ -88,7 +85,7 @@
         </div>
 
         <div class="index-body-detail">
-            <label for="index-body__detail" class="index-body__detail-label">お問い合わせの内容</label>
+            <label for="index-body__detail" class="index-body__detail-label">お問い合わせの内容<span class="index-body__asterisk">※</span></label>
             <div class="index-body__input-detail">
                 <textarea name="detail" id="index-body__detail" cols="30" rows="10">{{old('detail')}}</textarea>
             </div>
