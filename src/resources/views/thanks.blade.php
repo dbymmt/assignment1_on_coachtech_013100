9ed1d775d09c9471b8483d196903e03fa30884f9
dbymmt <dbymmt@dbtstdomoutno01.com>
2024-02-05 00:33:59 +0900

    in_progress_front_2402050033

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,2 +1,16 @@
-<h4>お問い合わせありがとうございました</h4>
-<a href="/">HOME</a>
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+@endsection
+
+@section('content')
+<article class="thanks-main">
+    <section class="thanks-body">
+        <h4>お問い合わせありがとうございました</h4>
+        <a href="/">HOME</a>
+    </section>
+</article>
+@endsection
+
+
